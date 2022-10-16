5728b05e2bfae92f2ef48d35eda3b6ac65704aa2
Joel Bill Agyeman-Mensah <joelagyeman@meltwater.org>
2022-10-16 13:36:49 +0000

    added third method

--- FirstStatic.java
+++ FirstStatic.java
@@ -1,4 +1,17 @@
 //this is my first static method
 public class FirstStatic {
+
+    public static void WhileTest() {
+        int sum = 0;
+        int i = 1;
+
+        while (i <= 1000);
+            sum += i;
+            ++i;
+            System.out.println(sum);
+            
+        }
+        
+    }
     
-}
+
