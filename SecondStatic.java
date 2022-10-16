af58d91472a1c04f5ca2cb86fbe5a57c08a5b2b6
Joel Bill Agyeman-Mensah <joelagyeman@meltwater.org>
2022-10-16 14:37:33 +0000

    fourth method works

--- SecondStatic.java
+++ SecondStatic.java
@@ -1,7 +1,13 @@
 //This is my static method
 public class SecondStatic {
 
-    public void name() {
+    public static void EvenNumbers () {
+        int sum = 0;
+        for (int i = 12; i<=103; i++){
+            if (i %2 ==0)
+            sum+=i;
+            System.out.println(sum);
+        }
         
     }
     
