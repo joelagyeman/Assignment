df7f912dc5a24d746b712a85c73042833aed7d8e
Joel Bill Agyeman-Mensah <joelagyeman@meltwater.org>
2022-10-16 13:25:15 +0000

    added a least 3 blocks

--- SecondDynamic.java
+++ SecondDynamic.java
@@ -1,4 +1,21 @@
 //This is my second dynamic method
-public void name() {
+public class SecondDynamic {
+
+public void Conditions() {
+    int marks = 100;
+    if (marks >= 90) {
+        System.out.println("Congrats! You have an A!");
+    } else if (marks > 80) {
+        System.out.println("You have an A!");
+    } else if (marks >= 70) {
+        System.out.println("You have an B!");
+    } else if (marks >= 40) {
+        System.out.println("You have passed!");
+    } else  {
+        System.out.println("You have failed chale! Resit");
+    }
+
     
+    
+}
 }
